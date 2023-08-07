e778e719c32762564405ac325c3c78e1261ede29
studboo <csingh@streamft.com>
2023-08-07 08:56:54 +0530

    testing done

--- client/index.ts
+++ client/index.ts
@@ -5,7 +5,7 @@ import './polyfill';
 const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: 'http://localhost:3000',
+      url: 'http://localhost:3000/trpc',
     }),
   ],
 });
@@ -15,16 +15,26 @@ async function main() {
    * Inferring types
    */
   const users = await trpc.userList.query();
-  //    ^?
+
   console.log('Users:', users);
 
-  const createdUser = await trpc.userCreate.mutate({ name: 'sachinraja' });
-  //    ^?
+  const createdUser = await trpc.userCreate.mutate({
+    name: 'sachinraja',
+    age: 25,
+    gender: 'M',
+  });
+
   console.log('Created user:', createdUser);
 
   const user = await trpc.userById.query('1');
-  //    ^?
+
   console.log('User 1:', user);
 }
 
-main().catch(console.error);
+main().catch((err) => {
+  console.error(
+    'An unexpected error occurred: -------------------------------',
+  );
+  console.error(err);
+  process.exit(1);
+});
--- server/index.ts
+++ server/index.ts
@@ -1,40 +1,74 @@
-import { createHTTPServer } from '@trpc/server/adapters/standalone';
 import { z } from 'zod';
 import { db } from './db';
 import { publicProcedure, router } from './trpc';
+import { inferAsyncReturnType, initTRPC } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import express from 'express';
 
-const appRouter = router({
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => {
+  return {
+    req,
+    res,
+    db,
+  };
+};
+
+export const appRouter = router({
   userList: publicProcedure.query(async () => {
     // Retrieve users from a datasource, this is an imaginary database
     const users = await db.user.findMany();
-    //    ^?
     return users;
   }),
   userById: publicProcedure.input(z.string()).query(async (opts) => {
     const { input } = opts;
-    //      ^?
     // Retrieve the user with the given ID
     const user = await db.user.findById(input);
     return user;
   }),
   userCreate: publicProcedure
-    .input(z.object({ name: z.string() }))
+    .input(
+      z.object({
+        name: z.string(),
+        age: z.number(),
+        gender: z.enum(['M', 'F']), // 'M' for male, 'F' for female
+      }),
+    )
     .mutation(async (opts) => {
       const { input } = opts;
-      //      ^?
-      // Create a new user in the database
       const user = await db.user.create(input);
-      //    ^?
       return user;
     }),
 });
 
-// Export type router type signature,
-// NOT the router itself.
 export type AppRouter = typeof appRouter;
 
-const server = createHTTPServer({
-  router: appRouter,
+type Context = inferAsyncReturnType<typeof createContext>;
+
+// Create a new TRPC instance
+const t = initTRPC.context<Context>().create();
+
+const app = express();
+
+app.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>TRPC Example</h1>
+    <p>Try <a href="/trpc/userList">/trpc/userList</a></p>
+    <p>Try <a href="/trpc/userById?input=1">/trpc/userById?input=1</a></p>
+    <p>Try <a href="/trpc/userCreate?input[name]=John">/trpc/userCreate?input[name]=John</a></p>
+  `);
 });
 
-server.listen(3000);
+app.listen(3000, () => {
+  console.log('Listening on http://localhost:3000/trpc');
+});
